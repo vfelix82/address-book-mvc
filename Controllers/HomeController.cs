8186674a0ac1fde09c71898fb0a27b1f095d1a54
Victor Felix <vfelix82@gmail.com>
2018-02-09 12:01:07 -0800

    add cshtml format and Controller format

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,26 @@ namespace AddressBook.Controllers
 {
     public class HomeController : Controller
     {
-        
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("/Form")]
+        public ActionResult GetForm()
+        {
+            List<Address> allAddress = Address.GetAll();
+            return View("Form", allAddress);
+        }
+        [HttpPost("/Form")]
+        public ActionResult Form()
+        {
+            string nameIn = Request.Form["name"];
+            string phoneNumberIn = Request.Form["phonenumber"];
+            string addressIn = Request.Form["address"];
+            Address New = new Address(nameIn, phoneNumberIn, addressIn);
+            List<Address> allAddress = Address.GetAll();
+            return View(allAddress);
+        }
     }
 }
