ce3a8fb8cf3bc73cbea5c3dc2d62cdeb2d3888e8
Victor Felix <vfelix82@gmail.com>
2018-02-09 08:42:51 -0800

    add MVC skeleton of AddressBook.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using AddressBook.Models;
+using System.Collections.Generic;
+
+namespace AddressBook.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+    }
+}
---
+++ Models/Address.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using System;
+namespace AddressBook.Models
+{
+    public class Address
+    {
+
+    }
+}
